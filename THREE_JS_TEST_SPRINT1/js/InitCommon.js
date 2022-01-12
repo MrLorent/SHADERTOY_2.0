45ca9953e7ea1ce2dc45781a90db596a066733b0
Yaelle <yaellesaraleguitran@gmail.com>
2022-01-12 12:37:00 +0100

    [Clean and functional test]

--- THREE_JS_TEST_SPRINT1/js/InitCommon.js
+++ THREE_JS_TEST_SPRINT1/js/InitCommon.js
@@ -2,55 +2,32 @@ let SCREEN_WIDTH;
 let SCREEN_HEIGHT;
 let canvas, context;
 let container;
-let controls;
-let pathTracingScene, screenCopyScene, screenOutputScene;
-let pathTracingUniforms, screenCopyUniforms, screenOutputUniforms;
+let pathTracingScene;
+let pathTracingUniforms;
 let pathTracingDefines;
 let pathTracingVertexShader, pathTracingFragmentShader;
-let screenCopyVertexShader, screenCopyFragmentShader;
-let screenOutputVertexShader, screenOutputFragmentShader;
 let pathTracingGeometry, pathTracingMaterial, pathTracingMesh;
-let screenCopyGeometry, screenCopyMaterial, screenCopyMesh;
-let screenOutputGeometry, screenOutputMaterial, screenOutputMesh;
-let pathTracingRenderTarget, screenCopyRenderTarget;
-let quadCamera, worldCamera;
+let pathTracingRenderTarget;
+let worldCamera;
 let renderer, clock;
 let frameTime, elapsedTime;
-let fovScale;
-let increaseFOV = false;
-let decreaseFOV = false;
-let dollyCameraIn = false;
-let dollyCameraOut = false;
 
-///aucune idée si ça sert
-let apertureSize = 0.0;
-let increaseAperture = false;
-let decreaseAperture = false;
 
 let pixelRatio = 0.5;
 let windowIsBeingResized = false;
 let TWO_PI = Math.PI * 2;
-let sampleCounter = 0.0; // will get increased by 1 in animation loop before rendering
 let frameCounter = 1.0; // 1 instead of 0 because it is used as a rng() seed in pathtracing shader
-let cameraIsMoving = false;
 let cameraRecentlyMoving = false;
 let isPaused = true;
 let oldYawRotation, oldPitchRotation;
 let mobileJoystickControls = null;
-let oldDeltaX = 0;
-let oldDeltaY = 0;
-let newDeltaX = 0;
-let newDeltaY = 0;
 let mobileControlsMoveX = 0;
 let mobileControlsMoveY = 0;
 let oldPinchWidthX = 0;
 let oldPinchWidthY = 0;
-let pinchDeltaX = 0;
-let pinchDeltaY = 0;
 let fontAspect;
 let useGenericInput = true;
 let EPS_intersect;
-let blueNoiseTexture;
 let cameraRotationSpeed = 1;
 let useToneMapping = true;
 let display = false;
@@ -60,9 +37,6 @@ let cameraDirectionVector = new THREE.Vector3(); //for moving where the camera i
 let cameraRightVector = new THREE.Vector3(); //for strafing the camera right and left
 let cameraUpVector = new THREE.Vector3(); //for moving camera up and down
 //let cameraWorldQuaternion = new THREE.Quaternion(); //for rotating scene objects to match camera's current rotation
-let cameraControlsObject; //for positioning and moving the camera itself
-let cameraControlsYawObject; //allows access to control camera's left/right movements through mobile input
-let cameraControlsPitchObject; //allows access to control camera's up/down movements through mobile input
 
 let PI_2 = Math.PI / 2; //used by controls below
 
@@ -152,17 +126,10 @@ function onWindowResize(event)
 	pathTracingUniforms.uResolution.value.y = context.drawingBufferHeight;
 
 	pathTracingRenderTarget.setSize(context.drawingBufferWidth, context.drawingBufferHeight);
-	screenCopyRenderTarget.setSize(context.drawingBufferWidth, context.drawingBufferHeight);
 
 	worldCamera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
 	worldCamera.updateProjectionMatrix();
 
-	// the following scales all scene objects by the worldCamera's field of view,
-	// taking into account the screen aspect ratio and multiplying the uniform uULen,
-	// the x-coordinate, by this ratio
-	fovScale = worldCamera.fov * 0.5 * (Math.PI / 180.0);
-	pathTracingUniforms.uVLen.value = Math.tan(fovScale);
-	pathTracingUniforms.uULen.value = pathTracingUniforms.uVLen.value * worldCamera.aspect;
 } // end function onWindowResize( event )
 
 
@@ -258,29 +225,13 @@ function initTHREEjs()
 	clock = new THREE.Clock();
 
 	pathTracingScene = new THREE.Scene();
-	screenCopyScene = new THREE.Scene();
-	screenOutputScene = new THREE.Scene();
-
-	// quadCamera is simply the camera to help render the full screen quad (2 triangles),
-	// hence the name.  It is an Orthographic camera that sits facing the view plane, which serves as
-	// the window into our 3d world. This camera will not move or rotate for the duration of the app.
-	quadCamera = new THREE.PerspectiveCamera(60, document.body.clientWidth / document.body.clientHeight, 1, 1000);//new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
-	screenCopyScene.add(quadCamera);
-	screenOutputScene.add(quadCamera);
 
 	// worldCamera is the dynamic camera 3d object that will be positioned, oriented and 
 	// constantly updated inside the 3d scene.  Its view will ultimately get passed back to the 
 	// stationary quadCamera, which renders the scene to a fullscreen quad (made up of 2 large triangles).
 	worldCamera = new THREE.PerspectiveCamera(60, document.body.clientWidth / document.body.clientHeight, 1, 1000);
 	pathTracingScene.add(worldCamera);
-
-	controls = new FirstPersonCameraControls(worldCamera);
-
-	cameraControlsObject = controls.getObject();
-	cameraControlsYawObject = controls.getYawObject();
-	cameraControlsPitchObject = controls.getPitchObject();
-
-	pathTracingScene.add(cameraControlsObject);
+	worldCamera.position.y=1;
 
 
 	// setup render targets...
@@ -294,25 +245,6 @@ function initTHREEjs()
 	});
 	pathTracingRenderTarget.texture.generateMipmaps = false;
 
-	screenCopyRenderTarget = new THREE.WebGLRenderTarget(context.drawingBufferWidth, context.drawingBufferHeight, {
-		minFilter: THREE.NearestFilter,
-		magFilter: THREE.NearestFilter,
-		format: THREE.RGBAFormat,
-		type: THREE.FloatType,
-		depthBuffer: false,
-		stencilBuffer: false
-	});
-	screenCopyRenderTarget.texture.generateMipmaps = false;
-
-	// blueNoise texture used in all demos
-	blueNoiseTexture = new THREE.TextureLoader().load('textures/BlueNoise_RGBA256.png');
-	blueNoiseTexture.wrapS = THREE.RepeatWrapping;
-	blueNoiseTexture.wrapT = THREE.RepeatWrapping;
-	blueNoiseTexture.flipY = false;
-	blueNoiseTexture.minFilter = THREE.NearestFilter;
-	blueNoiseTexture.magFilter = THREE.NearestFilter;
-	blueNoiseTexture.generateMipmaps = false;
-
 
 	// setup scene/demo-specific objects, variables, and data
 	initSceneData();
@@ -325,79 +257,14 @@ function initTHREEjs()
 	//ces variables ne servent peut être pas ??
 	pathTracingUniforms = {
 
-		tPreviousTexture: { type: "t", value: screenCopyRenderTarget.texture },
-		tBlueNoiseTexture: { type: "t", value: blueNoiseTexture },
+		//uSceneIsDynamic: { type: "b1", value: sceneIsDynamic },
 
-		uCameraIsMoving: { type: "b1", value: false },
-		uSceneIsDynamic: { type: "b1", value: sceneIsDynamic },
-
-		uEPS_intersect: { type: "f", value: EPS_intersect },
 		uTime: { type: "f", value: 0.0 },
-		uSampleCounter: { type: "f", value: 0.0 },
-		uFrameCounter: { type: "f", value: 1.0 },
-		uULen: { type: "f", value: 1.0 },
-		uVLen: { type: "f", value: 1.0 },
-		uApertureSize: { type: "f", value: apertureSize },
-
 		uResolution: { type: "v2", value: new THREE.Vector2() },
-		uRandomVec2: { type: "v2", value: new THREE.Vector2() },
-
-		uCameraMatrix: { type: "m4", value: new THREE.Matrix4() }
+		uCameraPosition: { type: "v3", value: new THREE.Vector3() }
 	};
 
 	initPathTracingShaders();
-
-
-	// this full-screen quad mesh copies the image output of the pathtracing shader and feeds it back in to that shader as a 'previousTexture'
-	screenCopyGeometry = new THREE.PlaneBufferGeometry(2, 2);
-
-	screenCopyUniforms = {
-		tPathTracedImageTexture: { type: "t", value: pathTracingRenderTarget.texture }
-	};
-
-	fileLoader.load('shaders/ScreenCopy_Fragment.glsl', function (shaderText) {
-		
-		screenCopyFragmentShader = shaderText;
-
-		screenCopyMaterial = new THREE.ShaderMaterial({
-			uniforms: screenCopyUniforms,
-			vertexShader: pathTracingVertexShader,
-			fragmentShader: screenCopyFragmentShader,
-			depthWrite: false,
-			depthTest: false
-		});
-
-		screenCopyMesh = new THREE.Mesh(screenCopyGeometry, screenCopyMaterial);
-		screenCopyScene.add(screenCopyMesh);
-	});
-
-
-	// this full-screen quad mesh takes the image output of the path tracing shader (which is a continuous blend of the previous frame and current frame),
-	// and applies gamma correction (which brightens the entire image), and then displays the final accumulated rendering to the screen
-	screenOutputGeometry = new THREE.PlaneBufferGeometry(2, 2);
-
-	screenOutputUniforms = {
-		uOneOverSampleCounter: { type: "f", value: 0.0 },
-		tPathTracedImageTexture: { type: "t", value: pathTracingRenderTarget.texture },
-		uUseToneMapping: { type: "b1", value: useToneMapping }
-	};
-
-	fileLoader.load('shaders/ScreenOutput_Fragment.glsl', function (shaderText) {
-
-		screenOutputFragmentShader = shaderText;
-
-		screenOutputMaterial = new THREE.ShaderMaterial({
-			uniforms: screenOutputUniforms,
-			vertexShader: pathTracingVertexShader,
-			fragmentShader: screenOutputFragmentShader,
-			depthWrite: false,
-			depthTest: false
-		});
-
-		screenOutputMesh = new THREE.Mesh(screenOutputGeometry, screenOutputMaterial);
-		screenOutputScene.add(screenOutputMesh);
-	});
-
 
 	// this 'jumpstarts' the initial dimensions and parameters for the window and renderer
 	onWindowResize();
@@ -417,108 +284,12 @@ function animate()
 
 	elapsedTime = clock.getElapsedTime() % 1000;
 
-	// reset flags
-	cameraIsMoving = false;
 
 	if (windowIsBeingResized)
 	{
-		cameraIsMoving = true;
 		windowIsBeingResized = false;
 	}
 
-	// check user controls
-	if (mouseControl)
-	{
-		// movement detected
-		if (oldYawRotation != cameraControlsYawObject.rotation.y ||
-			oldPitchRotation != cameraControlsPitchObject.rotation.x)
-		{
-			cameraIsMoving = true;
-		}
-
-		// save state for next frame
-		oldYawRotation = cameraControlsYawObject.rotation.y;
-		oldPitchRotation = cameraControlsPitchObject.rotation.x;
-
-	} // end if (mouseControl)
-
-	// if on mobile device, get input from the mobileJoystickControls
-	if (!mouseControl)
-	{
-
-		newDeltaX = joystickDeltaX * cameraRotationSpeed;
-
-		if (newDeltaX)
-		{
-			cameraIsMoving = true;
-			mobileControlsMoveX = oldDeltaX - newDeltaX;
-			// mobileJoystick X movement (left and right) affects camera rotation around the Y axis	
-			cameraControlsYawObject.rotation.y += (mobileControlsMoveX) * 0.01;
-		}
-
-		newDeltaY = joystickDeltaY * cameraRotationSpeed;
-
-		if (newDeltaY)
-		{
-			cameraIsMoving = true;
-			mobileControlsMoveY = oldDeltaY - newDeltaY;
-			// mobileJoystick Y movement (up and down) affects camera rotation around the X axis	
-			cameraControlsPitchObject.rotation.x += (mobileControlsMoveY) * 0.01;
-		}
-
-		// clamp the camera's vertical movement (around the x-axis) to the scene's 'ceiling' and 'floor',
-		// so you can't accidentally flip the camera upside down
-		cameraControlsPitchObject.rotation.x = Math.max(-PI_2, Math.min(PI_2, cameraControlsPitchObject.rotation.x));
-
-		// save state for next frame
-		oldDeltaX = newDeltaX;
-		oldDeltaY = newDeltaY;
-
-		newPinchWidthX = pinchWidthX;
-		newPinchWidthY = pinchWidthY;
-		pinchDeltaX = newPinchWidthX - oldPinchWidthX;
-		pinchDeltaY = newPinchWidthY - oldPinchWidthY;
-
-		if (Math.abs(pinchDeltaX) > Math.abs(pinchDeltaY))
-		{
-			if (pinchDeltaX < -1)
-			{
-				increaseFOV = true;
-				dollyCameraOut = true;
-			}
-			if (pinchDeltaX > 1)
-			{
-				decreaseFOV = true;
-				dollyCameraIn = true;
-			}
-		}
-
-		if (Math.abs(pinchDeltaY) >= Math.abs(pinchDeltaX))
-		{
-			if (pinchDeltaY > 1)
-			{
-				increaseAperture = true;
-			}
-			if (pinchDeltaY < -1)
-			{
-				decreaseAperture = true;
-			}
-		}
-
-		// save state for next frame
-		oldPinchWidthX = newPinchWidthX;
-		oldPinchWidthY = newPinchWidthY;
-
-	} // end if ( !mouseControl )
-
-	// this gives us a vector in the direction that the camera is pointing,
-	// which will be useful for moving the camera 'forward' and shooting projectiles in that direction
-	controls.getDirection(cameraDirectionVector);
-	cameraDirectionVector.normalize();
-	controls.getUpVector(cameraUpVector);
-	cameraUpVector.normalize();
-	controls.getRightVector(cameraRightVector);
-	cameraRightVector.normalize();
 
 	// the following gives us a rotation quaternion (4D vector), which will be useful for 
 	// rotating scene objects to match the camera's rotation
@@ -531,90 +302,29 @@ function animate()
 		{
 			if ( (keyPressed('w') || button3Pressed) && !(keyPressed('x') || button4Pressed) )
 			{
-				cameraControlsObject.position.add(cameraDirectionVector.multiplyScalar(camFlightSpeed * frameTime));
-				cameraIsMoving = true;
+				// cameraControlsObject.position.add(cameraDirectionVector.multiplyScalar(camFlightSpeed * frameTime));
 			}
 			if ( (keyPressed('x') || button4Pressed) && !(keyPressed('w') || button3Pressed) )
 			{
-				cameraControlsObject.position.sub(cameraDirectionVector.multiplyScalar(camFlightSpeed * frameTime));
-				cameraIsMoving = true;
+				// cameraControlsObject.position.sub(cameraDirectionVector.multiplyScalar(camFlightSpeed * frameTime));
 			}
 			if ( (keyPressed('q') || button1Pressed) && !(keyPressed('d') || button2Pressed) )
 			{
-				cameraControlsObject.position.sub(cameraRightVector.multiplyScalar(camFlightSpeed * frameTime));
-				cameraIsMoving = true;
+				// cameraControlsObject.position.sub(cameraRightVector.multiplyScalar(camFlightSpeed * frameTime));
 			}
 			if ( (keyPressed('d') || button2Pressed) && !(keyPressed('q') || button1Pressed) )
 			{
-				cameraControlsObject.position.add(cameraRightVector.multiplyScalar(camFlightSpeed * frameTime));
-				cameraIsMoving = true;
+				// cameraControlsObject.position.add(cameraRightVector.multiplyScalar(camFlightSpeed * frameTime));
 			}
 			if (keyPressed('z') && !keyPressed('s'))
 			{
-				cameraControlsObject.position.add(cameraUpVector.multiplyScalar(camFlightSpeed * frameTime));
-				cameraIsMoving = true;
+				// cameraControlsObject.position.add(cameraUpVector.multiplyScalar(camFlightSpeed * frameTime));
 			}
 			if (keyPressed('s') && !keyPressed('z'))
 			{
-				cameraControlsObject.position.sub(cameraUpVector.multiplyScalar(camFlightSpeed * frameTime));
-				cameraIsMoving = true;
-			}
-
-			if (keyPressed('right') && !keyPressed('left'))
-			{
-				increaseAperture = true;
-			}
-			if (keyPressed('left') && !keyPressed('right'))
-			{
-				decreaseAperture = true;
+				// cameraControlsObject.position.sub(cameraUpVector.multiplyScalar(camFlightSpeed * frameTime));
 			}
 		} // end if (!isPaused)
-
-		
-		
-		if (increaseFOV)
-		{
-			worldCamera.fov++;
-			if (worldCamera.fov > 150)
-				worldCamera.fov = 150;
-			fovScale = worldCamera.fov * 0.5 * (Math.PI / 180.0);
-			pathTracingUniforms.uVLen.value = Math.tan(fovScale);
-			pathTracingUniforms.uULen.value = pathTracingUniforms.uVLen.value * worldCamera.aspect;
-
-			cameraIsMoving = true;
-			increaseFOV = false;
-		}
-		if (decreaseFOV)
-		{
-			worldCamera.fov--;
-			if (worldCamera.fov < 1)
-				worldCamera.fov = 1;
-			fovScale = worldCamera.fov * 0.5 * (Math.PI / 180.0);
-			pathTracingUniforms.uVLen.value = Math.tan(fovScale);
-			pathTracingUniforms.uULen.value = pathTracingUniforms.uVLen.value * worldCamera.aspect;
-
-			cameraIsMoving = true;
-			decreaseFOV = false;
-		}
-
-		if (increaseAperture)
-		{
-			apertureSize += 0.1;
-			if (apertureSize > 100.0)
-				apertureSize = 100.0;
-			pathTracingUniforms.uApertureSize.value = apertureSize;
-			cameraIsMoving = true;
-			increaseAperture = false;
-		}
-		if (decreaseAperture)
-		{
-			apertureSize -= 0.1;
-			if (apertureSize < 0.0)
-				apertureSize = 0.0;
-			pathTracingUniforms.uApertureSize.value = apertureSize;
-			cameraIsMoving = true;
-			decreaseAperture = false;
-		}
 
 	} // end if (useGenericInput)
 
@@ -622,63 +332,17 @@ function animate()
 	// update scene/demo-specific input(if custom), variables and uniforms every animation frame
 	updateVariablesAndUniforms();
 
-	// now update uniforms that are common to all scenes
-	if (!cameraIsMoving)
-	{
-		if (sceneIsDynamic)
-			sampleCounter = 1.0; // reset for continuous updating of image
-		else sampleCounter += 1.0; // for progressive refinement of image
-
-		frameCounter += 1.0;
-
-		cameraRecentlyMoving = false;
-	}
-
-	if (cameraIsMoving)
-	{
-		sampleCounter = 1.0;
-		frameCounter += 1.0;
-
-		if (!cameraRecentlyMoving)
-		{
-			frameCounter = 1.0;
-			cameraRecentlyMoving = true;
-		}
-	}
-
 	pathTracingUniforms.uTime.value = elapsedTime;
-	pathTracingUniforms.uCameraIsMoving.value = cameraIsMoving;
-	pathTracingUniforms.uSampleCounter.value = sampleCounter;
-	pathTracingUniforms.uFrameCounter.value = frameCounter;
-	pathTracingUniforms.uRandomVec2.value.set(Math.random(), Math.random());
 
 	// CAMERA
-	cameraControlsObject.updateMatrixWorld(true);
 	worldCamera.updateMatrixWorld(true);
-	pathTracingUniforms.uCameraMatrix.value.copy(worldCamera.matrixWorld);
+	pathTracingUniforms.uCameraPosition.value.copy(worldCamera.position);
 
 	// PROGRESSIVE SAMPLE WEIGHT (reduces intensity of each successive animation frame's image)
-	screenOutputUniforms.uOneOverSampleCounter.value = 1.0 / sampleCounter;
 
-
-	// RENDERING in 3 steps
-
-	// STEP 1
-	// Perform PathTracing and Render(save) into pathTracingRenderTarget, a full-screen texture.
-	// Read previous screenCopyRenderTarget(via texelFetch inside fragment shader) to use as a new starting point to blend with
-	renderer.setRenderTarget(pathTracingRenderTarget);
+	renderer.setRenderTarget(null);
 	renderer.render(pathTracingScene, worldCamera);
 
-	// STEP 2
-	// Render(copy) the pathTracingScene output(pathTracingRenderTarget above) into screenCopyRenderTarget.
-	// This will be used as a new starting point for Step 1 above (essentially creating ping-pong buffers)
-
-
-	// STEP 3
-	// Render full screen quad with generated pathTracingRenderTarget in STEP 1 above.
-	// After applying tonemapping and gamma-correction to the image, it will be shown on the screen as the final accumulated output
-	renderer.setRenderTarget(null);
-	renderer.render(screenOutputScene, quadCamera);
 
 	// stats.update();
 
--- THREE_JS_TEST_SPRINT1/shaders/common_PathTracing_Vertex.glsl
+++ THREE_JS_TEST_SPRINT1/shaders/common_PathTracing_Vertex.glsl
@@ -1,7 +1,12 @@
 precision highp float;
 precision highp int;
 
+out vec3 fragCoord;
+out vec2 vuv;
+
 void main()
 {
+	fragCoord=position;
+	vuv=uv;
 	gl_Position = vec4( position, 1.0 );
 }
