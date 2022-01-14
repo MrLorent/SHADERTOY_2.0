4cffa5350ec7251496c4c68d87b810258f476f0c
TLORENT <lorent.tanguy@gmail.com>
2022-01-14 16:01:35 +0100

    [main] text editor test

--- SHADER_LAB/src/main.js
+++ SHADER_LAB/src/main.js
@@ -2,6 +2,9 @@ import './scss/index.scss';
 
 import { App } from './App.js'
 import { slider } from './html_generators/slider.jsx';
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/xml/xml.js';
+import 'codemirror/theme/blackboard.css';
 
 // APP
 const app = new App();
@@ -13,3 +16,15 @@ const inputs = document.getElementById('inputs');
 const tmp = slider(1,"Alpha", "input1",1,100);
 
 inputs.append(tmp);
+
+// Textarea
+const textarea = document.getElementById('editor');
+
+const config = CodeMirror.EditorConfiguration = {
+    tabSize: 3,
+    lineNumbers: true,
+    mode: 'xml',
+    theme: 'blackboard'
+};
+
+const editor = CodeMirror.fromTextArea(textarea, config);
