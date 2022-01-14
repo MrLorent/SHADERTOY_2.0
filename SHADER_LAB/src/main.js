922805899864db9fc1dabca269258e4fdec9e12e
TLORENT <lorent.tanguy@gmail.com>
2022-01-14 11:17:45 +0100

    [main] creating template

--- SHADER_LAB/src/main.js
+++ SHADER_LAB/src/main.js
@@ -1,12 +1,17 @@
 import './scss/index.scss';
 //import { App } from './App.js';
-import { slider } from './html_generators/slider.jsx';
 
 // APP
 // const app = new App();
 // app.run();
 
-// Inputs
+// CANVAS
+// import { maClass } from './fichierMaClasse'
+
+
+// INPUTS
+import { slider } from './html_generators/slider.jsx';
+
 const inputs = document.getElementById('inputs');
 
 const tmp = slider(1,"Alpha", "input1",1,100);
