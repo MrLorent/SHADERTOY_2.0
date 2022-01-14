c8650a4824df8366f7074f674eca9724c2da39f8
Yaelle <yaellesaraleguitran@gmail.com>
2022-01-14 12:42:12 +0100

    [App] moving 3D scene from main to App

--- SHADER_LAB/src/App.js
+++ SHADER_LAB/src/App.js
@@ -1,4 +1,14 @@
-import initCanvas from './canvas/InitCommon.js';
+import vertex from './shaders/vertex/vertexShader.glsl'
+import fragment from './shaders/fragment/phongIllumination.glsl'
+import Scene from './canvas/Scene.js'
+
+import init from './canvas/init.js'
+import * as GLOBALS from './canvas/globals.js'
+
+import Input from './canvas/Input.js';
+import Slider from './canvas/Slider.js'
+import Checkbox from './canvas/Checkbox.js'
+import Shader from './canvas/Shader.js'
 
 export class App
 {
@@ -6,6 +16,17 @@ export class App
 
     run()
     {
-        initCanvas();
+        let inputsShader = []
+        const checkbox = new Checkbox("Rotate light", "rotate_light", true);
+        const sliderTest = new Slider("Alpha", "alpha", 0,100,1);
+        inputsShader.push(checkbox);
+        inputsShader.push(sliderTest);
+
+        GLOBALS.shader = new Shader("Flat painting", vertex, fragment, inputsShader);
+        GLOBALS.scene = new Scene();
+        console.log(GLOBALS.shader)
+        console.log(GLOBALS.scene)
+
+        init(vertex, fragment);
     }
 }
