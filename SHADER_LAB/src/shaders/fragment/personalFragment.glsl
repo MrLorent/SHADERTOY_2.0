9200c5e1f629dcc2c3e6e535b8b2dd40393986b0
Yaelle <yaellesaraleguitran@gmail.com>
2022-01-25 13:18:00 +0100

    [Personal] hiding init_object

--- SHADER_LAB/src/shaders/fragment/personalFragment.glsl
+++ SHADER_LAB/src/shaders/fragment/personalFragment.glsl
@@ -10,10 +10,7 @@ in vec2 vertex_uv;
 #include <scene_preset_0>
 #include <RayMarch>   
 #include <rand>
-
-// To create an uniform value and to get a slider linked to it : 
-// /// color_picker scene uName name
-// /// slider scene uName name min max step
+#include <init_object_personal>
 
 /// checkbox light uRotatingLight rotate_light
 /// color_picker light uColorLight color_light
@@ -28,25 +25,9 @@ in vec2 vertex_uv;
 
 /// checkbox light uSecond_Light_on_off preset 
 
-void init_object(){
-    if(SCENE == 0){
-        sphere1.origin=vec3(-1,1,5);
-        sphere1.radius=0.5;
-
-        box1.origin=vec3(1,1,5);
-        box1.dimension=vec3(0.5);
-
-    }else if(SCENE == 1){
-        sphere1.origin=vec3(-1,1,5);
-        sphere1.radius=0.5;
-
-        sphere2.origin=vec3(0,1,5);
-        sphere2.radius=0.5;
-
-        sphere3.origin=vec3(1,1,5);
-        sphere3.radius=0.5;
-    }
-}
+// To create an uniform value and to get a slider linked to it : 
+// /// color_picker scene uName name
+// /// slider scene uName name min max step
 
 vec3 Model_Illumination(in vec3 ray_position, in vec3 ray_origin, in Material hit_object) {
 
