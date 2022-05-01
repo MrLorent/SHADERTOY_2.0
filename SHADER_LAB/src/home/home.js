b0c5d76381299cf62e4dd81632cf6d87a2320bf7
TLORENT <lorent.tanguy@gmail.com>
2022-05-01 02:38:13 +0200

    [home.scss] scrollable background

--- SHADER_LAB/src/home/home.js
+++ SHADER_LAB/src/home/home.js
@@ -1,19 +1,2 @@
 import '../commons/commons_scss.js';
 import './scss/home.scss'
-
-document.addEventListener('DOMContentLoaded', () => {
-  let main = document.querySelector('main');
-  main.addEventListener('scroll', move_background);
-
-  window.addEventListener('resize', move_background);
-});
-
-function move_background() {
-  const main = document.querySelector('main');
-  const NORMAL_RATIO = window.innerWidth / window.innerHeight;
-  const INVERSED_RATION = window.innerHeight / window.innerWidth;
-
-  let top_delta = main.scrollTop * 0.625 /
-      (NORMAL_RATIO < INVERSED_RATION ? NORMAL_RATIO : INVERSED_RATION);
-  main.style.backgroundPosition = 'top ' + -top_delta + 'px right 50%';
-}
